1ace41910d20fc268cc6d9fd0a78bc00ecd4f386
Ta Tan Phat <tatanphat.khtn@gmail.com>
2018-03-02 13:54:48 +0700

    Done and done

--- n-grams.py
+++ n-grams.py
@@ -1,12 +1,15 @@
 import numpy
 
+#use numpy only
+#data file with format, "sequence label"
 
-class glbClass: #singleton class, contains global properties
+class glbClass: #singleton class, contains global properties , just for encapsulation
     # Here will be the instance stored.
     __instance = None
-    __middleMatrix = []
+    __middleMatrix = [] #temp array for standardize data (padding zeros) 
 
-
+    data = numpy.array([]) #contain data read from file, type array of strings
+    target = numpy.array([],  dtype = str) # array of labels
     numberOfAttr = 0
     listOfAttr = {} #type: dictionary ,template: {attrName1: index,attrName2: index + 1 , ...}, attribute collection from extracting whole dataset 
     finalMatrix = numpy.array([]) # final result (usable data)
@@ -31,10 +34,17 @@ class glbClass: #singleton class, contains global properties
         return 1
     def saveToFile(self):
         self.__standardizeData()
-        numpy.savetxt('./output.txt', self.finalMatrix.astype(int),fmt='%i')
-        print 'Saved to ./output.txt'
-
-
+        numpy.savetxt('./data.txt', self.finalMatrix.astype(int),fmt='%i')
+        print 'Saved data to ./data.txt'
+        numpy.savetxt('./target.txt', self.target, fmt = '%s')
+        print 'Saved target to ./target.txt'
+    def readFromFile(self, fileName):
+        self.data = numpy.loadtxt(fileName, dtype = (str,str))
+    def appendTarget(self, label):
+        self.target = numpy.append(self.target, label)
+    # def numpyListOfAttr(self):
+    #     # dictKeys = self.
+    #     # return numpy.fromiter(self.listOfAttr.iterkeys() , dtype = 'S128')
     #---------------singleton implement------------ 
     @staticmethod
     def getInstance():
@@ -71,26 +81,33 @@ def ngram(ngramInstance, sequence, n):
             ngramInstance[attrIndex] = ngramInstance[attrIndex] + 1
     # print ngramInstance
     return ngramInstance
-def loopNgram(sequence):
+def loopNgram(dataTuple):
+
+    print dataTuple
+    sequence = dataTuple[0]
+    label = dataTuple[1]
+
     glbObj = glbClass.getInstance()
-    ngramInstance = numpy.zeros((glbObj.numberOfAttr), dtype=int)
+    ngramInstance = numpy.zeros((glbObj.numberOfAttr), dtype=int) #init ngramInstance with already-created attributes
     for i in range(1,40):
-        ngramInstance = ngram(ngramInstance, sequence, i)
-    return glbObj.appendRecord(ngramInstance)
+        ngramInstance = ngram(ngramInstance, sequence, i) 
+
+    glbObj.appendRecord(ngramInstance)
+    glbObj.appendTarget(label)
 
 glbObj = glbClass.getInstance()
-lst1 = loopNgram('PVCKPLLREEVEFQVGLNRYLVGSQLPCEPEPDVAVLTSM')
-lst2 = loopNgram('ACKPLLREEVVFQVGLNQYLVGSQLPCEPEPDVAVLTSML')
-
-# print len(listOfAttr)
-# print len(lst)
-# print listOfAttr
+# lst1 = loopNgram('PVCKPLLREEVEFQVGLNRYLVGSQLPCEPEPDVAVLTSM')
+# lst2 = loopNgram('ACKPLLREEVVFQVGLNQYLVGSQLPCEPEPDVAVLTSML')
 # glbObj.saveToFile()
-# print glbObj.
+# print glbObj.finalMatrix
+glbObj.readFromFile('./input.txt')
+
+for dataTuple in glbObj.data:
+    loopNgram(dataTuple)
+
+# print glbObj.target
 glbObj.saveToFile()
-print glbObj.finalMatrix
+# print glbObj.numpyListOfAttr()
 
-# PVCKPLLREEVEFQVGLNRYLVGSQLPCEPEPDVAVLTSM
 
-# ACKPLLREEVVFQVGLNQYLVGSQLPCEPEPDVAVLTSML
-
+print glbObj.listOfAttr
